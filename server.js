d1da624276d758b9d4488b4015fb6432ad318c4e
anindya033 <anindya033paul@gmail.com>
2025-06-01 13:23:29 +0530

    Heroku deployemnt changes

---
+++ server.js
@@ -0,0 +1,18 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+// Serve static files
+app.use(express.static(path.join(__dirname)));
+
+// For SPA (Angular routing)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
